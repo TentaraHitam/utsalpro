a88e6e2fe17bd9eabc584cad0ee5658d69a66f8a
Syahrul Ferdyansyah <115127417+TentaraHitam@users.noreply.github.com>
2022-11-24 11:09:43 +0700

    Create variabel.java

---
+++ variabel.java
@@ -0,0 +1,11 @@
+public class Main
+{
+    public static void main(String[] args) {
+        String nama = "Syahrul Ferdyansyah";
+        int umur = 2004;
+        
+        System.out.println("Assaamualaikum");
+        System.out.println("Nama saya : "+ nama);
+        System.out.println("saya lahir di tahun : "+ umur);
+    }
+}
